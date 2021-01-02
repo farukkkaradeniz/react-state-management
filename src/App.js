83ef20e58fd485022a55437aefdddbdd639929fb
TCFKARADENIZ <faruk.karadeniz@turkcell.com.tr>
2021-01-02 17:58:24 +0300

    added state

--- src/App.js
+++ src/App.js
@@ -2,14 +2,57 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const value1 = Math.floor(Math.random() * 100);
-const value2 = Math.floor(Math.random() * 100);
-const value3 = Math.floor(Math.random() * 100);
-const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
-const numQuestions = 0;
-const numCorrect = 0;
+const value1 = Math.floor(Math.random() * 10);
+const value2 = Math.floor(Math.random() * 10);
+const value3 = Math.floor(Math.random() * 10);
 
 class App extends Component {
+
+  state = {
+    value1 : value1,
+    value2 : value2,
+    value3 : value3,
+    proposedAnswer : Math.floor(Math.random() * 3) + value1 + value2 + value3,
+    numQuestions : 0,
+    numCorrect : 0
+  };
+
+  reloadValues = () => {
+    this.setState((prevState) => ({
+      value1 : Math.floor(Math.random() * 10),
+      value2 : Math.floor(Math.random() * 10),
+      value3 : Math.floor(Math.random() * 10),
+      proposedAnswer : Math.floor(Math.random() * 1) + prevState.value1 + prevState.value2 + prevState.value3
+    }));
+  }
+
+  checkAnswer = (answer) => {
+    if (this.state.proposedAnswer === (this.state.value1 + this.state.value2 + this.state.value3)) {
+      if (answer) {
+        this.setState((prevState) => ({
+          numCorrect : prevState.numCorrect + 1,
+          numQuestions : prevState.numQuestions + 1
+        }));
+      } else {
+        this.setState((prevState) => ({
+          numQuestions : prevState.numQuestions + 1
+        }));
+      }
+    } else {
+      if (answer) {
+        this.setState((prevState) => ({
+          numQuestions : prevState.numQuestions + 1
+        }));
+      } else {
+        this.setState((prevState) => ({
+          numCorrect : prevState.numCorrect + 1,
+          numQuestions : prevState.numQuestions + 1
+        }));
+      }
+    }
+    this.reloadValues();
+  }
+
   render() {
     return (
       <div className="App">
@@ -20,12 +63,12 @@ class App extends Component {
         <div className="game">
           <h2>Mental Math</h2>
           <div className="equation">
-            <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
+            <p className="text">{`${this.state.value1} + ${this.state.value2} + ${this.state.value3} = ${this.state.proposedAnswer}`}</p>
           </div>
-          <button>True</button>
-          <button>False</button>
+          <button onClick = {() => this.checkAnswer(true)}>True</button>
+          <button onClick = {() => this.checkAnswer(false)}>False</button>
           <p className="text">
-            Your Score: {numCorrect}/{numQuestions}
+            Your Score: {this.state.numCorrect}/{this.state.numQuestions}
           </p>
         </div>
       </div>
